82688bfece0c3b848b2530daf14b9654350ac182
Larry W. <l422y@l422y.com>
2024-01-08 14:23:52 -0500

    Refactor pg client to use connection pool for better performance

--- server/plugins/pg.ts
+++ server/plugins/pg.ts
@@ -1,40 +1,31 @@
-import pg from 'pg';
-const client = new pg.Client({
-  user: process.env.NUXT_PG_USER || 'reader',
-  host: process.env.NUXT_PG_HOST || 'hh-pgsql-public.ebi.ac.uk',
-  database: process.env.NUXT_PG_DB || 'pfmegrnargs',
-  password: process.env.NUXT_PG_PASSWORD || 'NWDMCE5xdipIjRrp',
-  port: 5432,
-});
-client
-  .connect()
-  .catch((reason) => {
-    console.error(reason);
-  })
-  .then((value) => {
-    console.log('connected', value);
-  });
+import pg from "pg"
 
-let connection;
 
-export const withPG = async (event: any, func: (dbClient: pg.Client) => {}) => {
-  return func.call(this, client);
-};
+// Defaults to RNA Central public database (https://rnacentral.org/help/public-database)
+const pool = new pg.Pool({
+    user: process.env.NUXT_PG_USER || "reader",
+    host: process.env.NUXT_PG_HOST || "hh-pgsql-public.ebi.ac.uk",
+    database: process.env.NUXT_PG_DB || "pfmegrnargs",
+    password: process.env.NUXT_PG_PASSWORD || "NWDMCE5xdipIjRrp",
+    port: 5432,
+    max: 5,
+})
+
+export const withPG = async (event: any, func: (dbClient: pg.PoolClient) => {}) => {
+    const client = await pool.connect() // get new client from pool
+    const result = func.call(this, client)
+    client.release()
+    return result // return client to pool
+}
 
 export default defineNitroPlugin(async (nitro) => {
-  // try {
-  //   await client.connect(function (err) {
-  //     if (err) {
-  //       console.error('DB CONNECT ERR', err);
-  //       throw err;
-  //     }
-  //     console.log('DB Connected!');
-  //   });
-  // } catch (err) {
-  //   console.error(err);
-  // }
-  // nitro.hooks.hookOnce('close', async () => {
-  //   await new Promise<void>((resolve) => client.end(() => resolve()));
-  //   console.log('db connection closed');
-  // });
-});
+
+    await pool.connect().then((value) => {
+        console.log("Postgres connection established with", value.database, "on", value.host)
+    })
+
+    nitro.hooks.hookOnce("close", async () => {
+        await new Promise<void>((resolve) => pool.end(() => resolve()))
+        console.log("db connection closed")
+    })
+})
