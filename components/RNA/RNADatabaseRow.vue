4fb41447e8b1be429fc51500f308fa57a8760f48
Larry W. <l422y@l422y.com>
2024-01-08 14:23:34 -0500

    Add RNADatabaseRow component with styling and props setup

---
+++ components/RNA/RNADatabaseRow.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="rna-database-row">
+        <time>{{ new Date(row.timestamp).toLocaleDateString() }}</time>
+        <div>
+            <a :href="row.url" target="_blank">{{ row.display_name }}</a>
+            {{ row.project_id ? `(${row.project_id})` : "" }}
+            <span v-html="row.description"></span>
+        </div>
+        <div>Sequences: {{ row.num_sequences }}, Organisms: {{ row.num_organisms }}</div>
+    </div>
+</template>
+<script lang="ts" setup>
+import type { RowInterface } from "~/types"
+
+defineProps({
+    row: {
+        type: Object as PropType<RowInterface>,
+        required: true
+    }
+})
+</script>
+<style scoped>
+.row {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+</style>
